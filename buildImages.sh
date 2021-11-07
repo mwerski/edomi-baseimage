12a9c0f0d2d804a1e9951c92cd26248bd3723920
Yves Schumann <y.schumann@yetnet.ch>
2021-11-07 21:53:39 +0100

    Refactored build helper script

--- buildImages.sh
+++ buildImages.sh
@@ -12,6 +12,8 @@
 callDir=$(pwd)
 ownLocation="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${ownLocation}
+. ./include/helpers_console.sh
+_init
 
 helpMe() {
     echo "
@@ -26,8 +28,62 @@ helpMe() {
     "
 }
 
+pullRocky() {
+    if [[ $PULL_ROCKYLINUX_IMAGE ]] ; then
+        info "Pulling ${ROCKYLINUX_VERSION}"
+        docker pull "${ROCKYLINUX_VERSION}"
+        info " -> Done"
+    else
+        info "Skipping pull of ${ROCKYLINUX_VERSION}"
+    fi
+}
+
+getDigests() {
+    info "Determining amd64 and arm64 image digests"
+    docker manifest inspect rockylinux/rockylinux:latest > /tmp/rockyLinuxManifest.json
+    DIGEST_AMD64=$(jq -j '.manifests[] | select(.platform.architecture == "amd64") | .digest' /tmp/rockyLinuxManifest.json)
+    DIGEST_ARM64=$(jq -j '.manifests[] | select(.platform.architecture == "arm64") | .digest' /tmp/rockyLinuxManifest.json)
+    info " -> amd64: ${DIGEST_AMD64}"
+    info " -> arm64: ${DIGEST_ARM64}"
+#    rm -f /tmp/rockyLinuxManifest.json
+}
+
+tagRockyImages() {
+    info "Taging rocky linux images"
+    docker pull "${ROCKYLINUX_VERSION}@${DIGEST_AMD64}"
+    docker pull "${ROCKYLINUX_VERSION}@${DIGEST_ARM64}"
+    docker tag "${ROCKYLINUX_VERSION}@${DIGEST_AMD64}" "${ROCKYLINUX_VERSION}-amd64"
+    docker tag "${ROCKYLINUX_VERSION}@${DIGEST_ARM64}" "${ROCKYLINUX_VERSION}-arm64"
+    info " -> Done"
+}
+
+buildBaseimage() {
+    local _arch=$1
+    info "Building starwarsfan/edomi-baseimage-builder:latest-${_arch}"
+    docker build -f "${_arch}.Builder.Dockerfile" -t "starwarsfan/edomi-baseimage-builder:latest-${_arch}" .
+    info " -> Done"
+    if ${PUBLISH_IMAGE} ; then
+        info "Pushing starwarsfan/edomi-baseimage-builder:latest-${_arch}"
+        docker push "starwarsfan/edomi-baseimage-builder:latest-${_arch}"
+        info " -> Done"
+    fi
+
+    info "Building starwarsfan/edomi-baseimage:latest-${_arch}"
+    docker build -f "${_arch}.Dockerfile" -t "starwarsfan/edomi-baseimage:latest-${_arch}" .
+    info " -> Done"
+    if ${PUBLISH_IMAGE} ; then
+        info "Pushing starwarsfan/edomi-baseimage:latest-${_arch}"
+        docker push "starwarsfan/edomi-baseimage:latest-${_arch}"
+        info " -> Done"
+    fi
+}
+
 PUBLISH_IMAGE=false
 BUILD_ARM_IMAGES=false
+PULL_ROCKYLINUX_IMAGE=true
+ROCKYLINUX_VERSION=rockylinux/rockylinux:latest
+DIGEST_AMD64=''
+DIGEST_ARM64=''
 
 while getopts aph? option; do
     case ${option} in
@@ -38,26 +94,10 @@ while getopts aph? option; do
     esac
 done
 
-docker build -f amd64.Builder.Dockerfile -t starwarsfan/edomi-baseimage-builder:amd64-latest .
-if ${PUBLISH_IMAGE} ; then
-    docker push starwarsfan/edomi-baseimage-builder:amd64-latest
-fi
-
-docker build -f amd64.Dockerfile -t starwarsfan/edomi-baseimage:amd64-latest .
-if ${PUBLISH_IMAGE} ; then
-    docker push starwarsfan/edomi-baseimage:amd64-latest
-fi
-
+pullRocky
+getDigests
+tagRockyImages
+buildBaseimage amd64
 if ${BUILD_ARM_IMAGES} ; then
-    docker build -f arm32v7.Builder.Dockerfile -t starwarsfan/edomi-baseimage-builder:arm32v7-latest .
-    if ${PUBLISH_IMAGE} ; then
-        echo "ARMv7 unsupported at the moment, no push to DockerHub :-/"
-    #    docker push starwarsfan/edomi-baseimage-builder:arm32v7-latest
-    fi
-
-    docker build -f arm32v7.Dockerfile -t starwarsfan/edomi-baseimage:arm32v7-latest .
-    if ${PUBLISH_IMAGE} ; then
-        echo "ARMv7 unsupported at the moment, no push to DockerHub :-/"
-    #    docker push starwarsfan/edomi-baseimage:arm32v7-latest
-    fi
+    buildBaseimage arm64
 fi
